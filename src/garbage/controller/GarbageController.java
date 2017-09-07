b04c607c7be8e3bd24d0ee3e60cde5e824ab4cfd
Aaron Montoya <aatoya7@gmail.com>
2017-09-07 12:28:42 -0600

    Created the Controller and Runner files including the main and start methods.

---
+++ src/garbage/controller/GarbageController.java
@@ -0,0 +1,10 @@
+package garbage.controller;
+
+public class GarbageController
+{
+	public void start()
+	{
+		System.out.println("This is a garbage project");
+		System.out.println("such a waste :(");
+	}
+}
---
+++ src/garbage/controller/GarbageRunner.java
@@ -0,0 +1,10 @@
+package garbage.controller;
+
+public class GarbageRunner
+{
+	public static void main (String [] args)
+	{
+		GarbageController app_controller = new GarbageController();
+		app_controller.start();
+	}
+}
